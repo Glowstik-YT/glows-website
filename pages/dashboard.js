ce12fcc20b8b8624d40e21bdbc1cf1c17d2e9ca4
AryamanP728 <aryamanp728@gmail.com>
2021-04-30 22:40:46 -0400

    ok im making a dashboard instead

---
+++ pages/dashboard.js
@@ -0,0 +1,14 @@
+import Head from 'next/head';
+import Image from "next/image";
+import styles from '../styles/Home.module.css';
+
+export default function lang() {
+  return (
+    <div> 
+
+      <style jsx>{`
+        
+      `}</style>
+    </div>
+  )
+}
